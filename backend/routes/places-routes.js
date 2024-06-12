936c37f5b36ab04e6525c1c6712f25454ec30bc3
Riyaz Rabbani <riyaz.rabbani@hotmail.com>
2024-06-12 18:30:54 +0000

    nodemon install

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -14,14 +14,23 @@ const DUMMY_PLACES = [
         address: '20 W 34th St, New York, NY 10001',
         creator: 'u1'
     }
-]       
+]
 
 router.get('/:pid', (req, res, next) => {
     const placeId = req.params.pid;
     const place = DUMMY_PLACES.find(p => {
         return p.id === placeId;
     });
-    res.json({place});
+    res.json({ place });
 });
+
+router.get('/user/:uid', (req, res, next) => {
+    const userId = req.params.uid;
+
+    const place = DUMMY_PLACES.find(p => {
+        return p.creator === userId;
+    })
+    res.json({ place })
+})
 
 module.exports = router;
